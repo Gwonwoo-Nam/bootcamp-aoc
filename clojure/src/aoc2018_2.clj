07f95dd38ca5fece923c5a80e85308183a8850e2
Gwonwoo Nam <gwonwoo.nam@greenlabs.co.kr>
2023-10-06 16:36:39 +0900

    feat: 2018 Day 2, Day 3 문제 풀이

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -1,4 +1,5 @@
-(ns aoc2018-2)
+(ns aoc2018-2
+  (:require [clojure.string :as str]))
 
 ;; 파트 1
 ;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
@@ -14,8 +15,66 @@
 ;; 답 : 4 * 3 = 12
 
 
+(require '[clojure.string :as str])
+
+;; 문자열 파싱
+;; (defn parse-string [input]
+;;   (let [input (str/replace input " " "")]
+;;     (for [row (str/split input #"\n")] row)))
+
+;; 문자열 파싱
+(defn parse-string [input]
+  (->> (str/split input #"\n") 
+       (map #(str/trim %))))
+
+(comment
+  (parse-string "abcdef
+                         bababc
+                         ababab"))
+
+
+;; count가 2인 글자를 set에 추가
+
+;; 한 줄의 문자열에 들어있는 글자의 count 수가 2 혹은 3인 문자들 반환
+(defn count-chars [row count]
+  (set (keys (filter #(= (val %) count) (frequencies row)))))
+
+
+;; 한줄 단위로 
+
+;; 2 혹은 3개의 문자열들 저장하는 set
+;; 함수의 단위를 작게 나누어서 리팩토링 해보기
+;; pos, inc, seq, split-lines 등 core 함수
+;; 쓰레딩 매크로
+(defn get-repeated-charset [input c]
+  (->> input
+       (map #(count-chars % c))
+       (filter seq)
+       count))
+
+(defn count-repeated-chars [input]
+  (* 
+   (count (get-repeated-charset input 2)) 
+   (count (get-repeated-charset input 3))))
+
+
+(comment
+
+  (get-repeated-charset ["aabbcc"
+                         "bbcadf"
+                         "cadght"] 2)
+  (count-repeated-chars (parse-string "abcdef
+                                                bababc
+                                                abbcde
+                         abcccd
+                         aabcdd
+                         abcdee
+                         ababab"))
+)
+
 ;; 파트 2
 ;; 여러개의 문자열 중, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍에서 같은 부분만을 리턴하시오.
+
 ;; 예)
 ;; abcde
 ;; fghij
@@ -25,8 +84,73 @@
 ;; axcye
 ;; wvxyz
 
+
+
 ;; 주어진 예시에서 fguij와 fghij는 같은 위치 (2번째 인덱스)에 정확히 한 문자 (u와 h)가 다름. 따라서 같은 부분인 fgij를 리턴하면 됨.
 
+;; cartesian product으로 모든 경우의 수
+;; combination
+
+
+
+(defn count-diff [str1 str2]
+  (->>
+   (map #(= %1 %2) str1 str2)
+   (filter false?)
+   (count))
+)
+
+(defn nearly-same? [[str1 str2]]
+  (= (count-diff str1 str2) 1))
+
+(defn combinations [parsed-string]
+  (for [x parsed-string y parsed-string] [x y]))
+
+(defn find-nearly-same [str] 
+  (->>
+   (combinations (parse-string str))
+   (filter nearly-same?)
+   (first))
+)
+
+(defn conj-same [[str1 str2]]
+  (->>
+   (map (fn [c1 c2] (when (= c1 c2) c1)) str1 str2)
+   (filter (complement nil?))
+   (reduce str "")))
+
+(defn answer [str] 
+   (conj-same (find-nearly-same str)))
+
+
+(comment
+  (def parsed (parse-string "abcdef
+                        bababc
+                                                abbcde
+                                                abcccd
+                                                aabcdd
+                                                abcdee
+                                                ababab"))
+  (combinations parsed)
+  (count-diff "abc" "bbc")
+  (nearly-same? ["abc" "bbc"])
+  (find-nearly-same "abcdef
+                                            bababc
+                                                                    abbcde
+                                                                    abcccd
+                                                                    aabcdd
+                                                                    abcdee
+                                                                    ababab")
+  (conj-same ["abcdef" "abcdee"])
+
+  (answer "abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz")
+  )
 
 ;; #################################
 ;; ###        Refactoring        ###
--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,6 +1,14 @@
-(ns aoc2018_3)
+(ns aoc2018_3 
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
-
+(defn get-input-puzzle
+  "문제의 인풋 데이터를 읽은 다음 파싱된 값을 제공하는 함수
+   input: 인풋 데이터 파일 이름
+   output: newline 문자 기준으로 split된 리스트"
+  [filename]
+  (let [input-file (io/resource filename)]
+    (map #(Integer/parseInt %) (str/split (slurp input-file) #"\n"))))
 ;; 파트 1
 ;; 다음과 같은 입력이 주어짐.
 
@@ -22,11 +30,145 @@
 
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
+;; 라인별로 map을 만든다
 
 
+
+;; 입력을 라인별로 split된 벡터로 좌우 white space trimming하여 반환한다.
+(defn split-input-lines [input]
+  (->> input
+       (str/split-lines)
+       (map str/trim)))
+
+;; 불필요한 문자를 제거하고 (id, x, y ,width, height)로 반환한다.
+(defn split-by-format [input]
+  (->> input
+       (map #(str/split % #"@|:| |#|,|x"))
+       (map (fn [l] (remove #(= % "") l))) 
+       (map (fn [l] (map #(Integer/parseInt %) l)))))
+
+
+(defn input-stream [input]
+  (->> input
+       (split-input-lines)
+       (split-by-format)
+   )
+  )
+
+;; 입력 테스트
+(comment
+  (def puzzle "#1 @ 1,3: 4x4
+               #2 @ 3,1: 4x4
+            #3 @ 5,5: 2x2")
+
+  (split-input-lines puzzle)
+  (split-by-format (split-input-lines puzzle))
+  (input-stream puzzle)
+
+  (input-stream "#1 @ 1,3: 4x4
+                 #2 @ 3,1: 4x4
+              #3 @ 5,5: 2x2")
+  
+  )
+  
+;; x,y 좌표의 범위를 생성한다.
+(defn cartesian-product [x-loc y-loc width height]
+  (for [x (range x-loc (+ x-loc width)) y (range y-loc (+ y-loc height))]
+    [x y]))
+
+
+;; map에 좌표 : ID로 값을 저장한다.
+(defn mark-on-map [id x-loc y-loc width height]
+  (->> (cartesian-product x-loc y-loc width height)
+       (map (fn [[x y]] {:id id :coord [x y]}))))
+
+;; 생성된 map을 합친다. 이 때 중복되는 값이 있으면 count한다.
+;; 문제 참고해서 네이밍
+(defn merge-maps [parsed]
+  (->>
+   (map (fn [[id x-loc y-loc width height]] (mark-on-map id x-loc y-loc width height)) parsed)
+   (reduce #(concat %1 %2) {})))
+
+;; map의 좌표만 가져와서 빈도 분석
+(defn analyze [data]
+  (->> data
+       (map :coord) 
+       (frequencies)
+       (vals)
+       (filter #(> % 1))
+       (count)
+       ))
+
+;; 최종함수
+(defn output-stream [data]
+  (->> data
+       (input-stream)
+       (merge-maps)
+       (analyze)
+       )
+  )
+
+(analyze [{:id 3, :coord [6 6]} {:id 3, :coord [6 6]}])
+(filter (complement number?) (vals {:id 3, :coord [6 6]}))
+(comment
+  
+  (cartesian-product 1 3 4 4) 
+
+  (mark-on-map 1 1 3 4 4)
+
+  (first (mark-on-map 1 1 3 4 4))
+
+  (merge-maps parsed)
+
+  (analyze (merge-maps parsed))
+
+  (output-stream puzzle)
+)
 
 
 ;; 파트 2
 ;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
 ;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
 ;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
+
+;; map에 좌표, [id list]를 저장하여 id의 개수가 2개 이상인 것만 filter, id의 목록으로 추출한다.
+;; 전체 id의 목록에서 겹치는 id를 소거하여 안겹치는 id를 찾는다.
+
+(defn find-all-id [data]
+  (->> data
+       (map :id)
+       (set)))
+
+(defn find-overlapped-id [data]
+  (->> data
+       (group-by :coord) 
+       (vals)
+       (filter #(> (count %) 1))
+       (map (fn [l] (map :id l)))
+       (flatten)
+       (distinct)
+       (set)))
+
+(defn get-difference [data]
+  (first 
+   (clojure.set/difference
+    (find-all-id (merge-maps data))
+    (find-overlapped-id (merge-maps data)))))
+  
+(defn find-unique-patch [input]
+  (->> input
+       (input-stream)
+       (get-difference)))
+  
+(comment
+  (def data (input-stream "#1 @ 1,3: 4x4
+                   #2 @ 3,1: 4x4
+                #3 @ 5,5: 2x2"))
+  (merge-maps data)
+  (find-overlapped-id (merge-maps data))
+  (find-all-id (merge-maps data))
+  
+  (find-unique-patch "#1 @ 1,3: 4x4
+                                        #2 @ 3,1: 4x4
+                                     #3 @ 5,5: 2x2")
+  )
